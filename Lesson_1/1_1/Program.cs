b44bd2c9175b454c39d8c468162b78e7b2ba5bf6
celestialmary <mail@mail.ru>
2023-12-21 21:29:32 +0300

    Lesson 1 task 1 some changes

--- Lesson_1/1_1/Program.cs
+++ Lesson_1/1_1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет,
 //    является ли первое число квадратом второго.
 
+
 Console.Write("Enter a number 1: ");
 int b1 = int.Parse(Console.ReadLine()!);
 Console.Write("Enter a number 2: ");
